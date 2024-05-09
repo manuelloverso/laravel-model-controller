0a428045060c0dc8fe39809c3f8b46290ee2b77c
Manuel <manuelloverso60@gmail.com>
2024-05-09 14:38:31 +0200

    add view partials and layouts structure

--- app/Http/Controllers/Guests/PageController.php
+++ app/Http/Controllers/Guests/PageController.php
@@ -11,7 +11,6 @@ class PageController extends Controller
     public function index()
     {
         $movies = Movie::all();
-        dd($movies);
-        return view('home');
+        return view('home', compact('movies'));
     }
 }
