15f350cf7df54a8a8019de4f8e8f721acad3000b
Manuel <manuelloverso60@gmail.com>
2024-05-09 14:45:50 +0200

    print movies on page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>I am the home page</h1>
+    <div class="container">
+        <div class="row g-3">
+            @forelse ($movies as $movie)
+                <div class="col-4 ">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">{{$movie->title}}</h2>
+                            <h5><strong>Original Title: </strong>{{$movie->original_title}}</h5>
+                            <p><strong>Nationality: </strong>{{$movie->nationality}}</p>
+                            <p><strong>Debut date: </strong>{{$movie->date}}</p>
+                            <p><strong>Critics' Rating: </strong>{{$movie->vote}}</p>
+                        </div>
+                    </div>
+                </div>
+            @empty
+                <h1>No movies found</h1>
+            @endforelse
+        </div>
+    </div>
 @endsection
